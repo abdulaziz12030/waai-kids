0dedd7b4150e8534b78be09bc0ffd0c7c154de1b
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-09-04 22:30:41 +0300

    Create page.tsx

---
+++ app/services/page.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: "خدمات الطالب | واعي كيدز" };
+export default function ServicesPage() {
+  return <div className="card">هنا سنعرض المطويات والأنشطة والملخصات.</div>;
+}
