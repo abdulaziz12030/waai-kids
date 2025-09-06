11b21f6665d6b815f2d95f334a8f4a971209fa41
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-09-06 22:52:23 +0300

    Update gifts.js

--- pages/gifts.js
+++ pages/gifts.js
@@ -1,1 +1,16 @@
+import Link from "next/link";
+
+export default function Gifts() {
+  return (
+    <main style={{ direction: "rtl", padding: 40 }}>
+      <h2>هدايا المواليد</h2>
+      <ul>
+        <li>علب هدايا أطفال مميزة</li>
+        <li>ملابس مواليد فاخرة</li>
+        <li>مجسمات تذكار اسم الطفل</li>
+      </ul>
+      <Link href="/">العودة للرئيسية</Link>
+    </main>
+  );
+}
 
