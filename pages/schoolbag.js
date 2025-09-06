7db35dc44565324e1a589bf8597d8c9d75065f2e
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-09-06 22:53:10 +0300

    Update schoolbag.js

--- pages/schoolbag.js
+++ pages/schoolbag.js
@@ -1,1 +1,16 @@
+import Link from "next/link";
+
+export default function Schoolbag() {
+  return (
+    <main style={{ direction: "rtl", padding: 40 }}>
+      <h2>الحقيبة المدرسية</h2>
+      <ul>
+        <li>حقائب مدرسية بجودة عالية</li>
+        <li>أقلام وأدوات مدرسية</li>
+        <li>دفاتر ملونة وكراسات مميزة</li>
+      </ul>
+      <Link href="/">العودة للرئيسية</Link>
+    </main>
+  );
+}
 
