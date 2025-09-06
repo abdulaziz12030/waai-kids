63b9cf791801a248621547040e1cb6c17ee25de1
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-09-06 22:52:03 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main style={{ direction: "rtl", padding: 40 }}>
+      <h1>واعي كيدز - Waai Kids</h1>
+      <nav>
+        <ul style={{ listStyle: "none", padding: 0, fontSize: 20 }}>
+          <li><Link href="/gifts">🎁 هدايا المواليد</Link></li>
+          <li><Link href="/toys">🧸 الألعاب التعليمية</Link></li>
+          <li><Link href="/services">🔬 خدمات علمية للأطفال</Link></li>
+          <li><Link href="/schoolbag">🎒 الحقيبة المدرسية</Link></li>
+        </ul>
+      </nav>
+    </main>
+  );
+}
 
