a20123c40e69216aa6af02e8dd261e7d3fb1b68f
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-09-06 22:52:41 +0300

    Update toys.js

--- pages/toys.js
+++ pages/toys.js
@@ -1,1 +1,16 @@
+import Link from "next/link";
+
+export default function Toys() {
+  return (
+    <main style={{ direction: "rtl", padding: 40 }}>
+      <h2>الألعاب التعليمية للأطفال</h2>
+      <ul>
+        <li>لعبة تركيب الحروف</li>
+        <li>مكعبات الأرقام والألوان</li>
+        <li>أجهزة تعليم الحروف والأرقام</li>
+      </ul>
+      <Link href="/">العودة للرئيسية</Link>
+    </main>
+  );
+}
 
