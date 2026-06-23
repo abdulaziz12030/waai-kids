a46f799cb055d53e3b09c52ed6c1e9bbc93491ed
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-06-23 20:29:38 +0300

    style: load child account styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import './avatar-controls.css';
 import './dashboard-v2.css';
 import './motion.css';
 import './goals.css';
+import './child-app.css';
 
 export const metadata: Metadata = {
   title: 'نماء',
