196d55aede58a07bd2e28f32e05b3f963320a5fd
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-06-28 18:32:24 +0300

    feat: type child gift card

--- app/child/gifts/GiftCard.tsx
+++ app/child/gifts/GiftCard.tsx
@@ -1,3 +1,5 @@
-export default function GiftCard() {
-  return <div>هدية</div>;
+import { ChildGift } from "./types";
+
+export default function GiftCard({ gift }: { gift: ChildGift }) {
+  return <article>{gift.gift.icon} {gift.gift.name}</article>;
 }
