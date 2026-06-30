0b526f9a2fc191d0c7b29275e3f51131b004e485
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-06-30 14:57:18 +0300

    security: restrict restart RPCs to authenticated users

---
+++ supabase/migrations/20260630145500_restrict_restart_rpcs_to_authenticated.sql
@@ -0,0 +1,3 @@
+revoke all privileges on function public.parent_zero_student_achievement_points(uuid) from anon;
+revoke all privileges on function public.parent_delete_all_student_goals(uuid) from anon;
+revoke all privileges on function public.reset_student_quran_program_shared(uuid) from anon;
