e8262d088180282f328b9415967b5a38f60956b5
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-07-02 01:49:30 +0300

    ui: show child journey coach on dashboard

--- app/child/layout.tsx
+++ app/child/layout.tsx
@@ -1,7 +1,8 @@
 import ChildExperienceLayer from "./ChildExperienceLayer";
 import ChildGiftAutoDisplay from "./ChildGiftAutoDisplay";
+import ChildJourneyCoach from "./ChildJourneyCoach";
 import ChildSectionNav from "./ChildSectionNav";
 
 export default function ChildLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<ChildExperienceLayer /><ChildGiftAutoDisplay /><ChildSectionNav /></>;
+  return <>{children}<ChildExperienceLayer /><ChildGiftAutoDisplay /><ChildJourneyCoach /><ChildSectionNav /></>;
 }
