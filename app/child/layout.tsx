b4f5f88a311fbbeb296b9bca67e06ff15c4c4f96
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-07-03 17:07:02 +0300

    Load child dashboard cleanup styles

--- app/child/layout.tsx
+++ app/child/layout.tsx
@@ -1,5 +1,6 @@
 import "./child-simplified.css";
 import "./child-dashboard-v3.css";
+import "./child-layout-cleanup.css";
 import ChildExperienceLayer from "./ChildExperienceLayer";
 import ChildGiftAutoDisplay from "./ChildGiftAutoDisplay";
 import ChildSectionNav from "./ChildSectionNav";
