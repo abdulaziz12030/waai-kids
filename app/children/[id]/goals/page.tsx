a64618491d6a82f8079b8277be293f81c76c3497
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2026-07-03 19:39:26 +0300

    Add Quran goal conversion to parent goals page

--- app/children/[id]/goals/page.tsx
+++ app/children/[id]/goals/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import ChildGoals from "../../../components/ChildGoals";
 import GoalDeletionManager from "../../../components/GoalDeletionManager";
+import GoalQuranFormBridge from "../../../components/GoalQuranFormBridge";
+import GoalQuranTaskPlanner from "../../../components/GoalQuranTaskPlanner";
 
 export default function ChildGoalsPage() {
   const params = useParams<{ id: string }>();
@@ -13,12 +15,14 @@ export default function ChildGoalsPage() {
     <main className="child-profile-page">
       <header className="dashboard-header">
         <Link className="brand" href="/dashboard">
-          <span className="brand-mark">ن</span>
-          <span>نماء</span>
+          <span className="brand-mark">و</span>
+          <span>واعي كيدز</span>
         </Link>
         <Link className="quiet-button link-submit" href={`/children/${studentId}`}>ملف الطفل</Link>
       </header>
 
+      <GoalQuranFormBridge />
+      <GoalQuranTaskPlanner studentId={studentId} />
       <GoalDeletionManager studentId={studentId} />
       <ChildGoals studentId={studentId} />
     </main>
